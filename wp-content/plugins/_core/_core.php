8c36ff831f039d8c38cd5007d7ffc519a96f7d52
Mizner <mike@mizner.io>
2019-08-26 07:15:44 -0400

    initial wp files

---
+++ wp-content/plugins/_core/_core.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name: (custom) Core Plugin
+ * Description: Plugin for custom WordPress development
+ * Author: Michael Mizner
+ * Author URI: https://mizner.io
+ * License: GPLv3+
+ */
+
+namespace _core;
+
+defined('WPINC') || die;
+
+define(__NAMESPACE__ . '\PATH', plugin_dir_path(__FILE__));
+define(__NAMESPACE__ . '\URI', plugin_dir_url(__FILE__));
